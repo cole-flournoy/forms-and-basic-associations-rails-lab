69f8661b0c742261cec9f9f677e5ed5e0f578257
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-28 01:31:28 -0500

    commit tree 8a25f7f0a3b6db730bca5838cdc2db6f95f3b622

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_content=(notes)
+    notes.each do |note|
+      if !note.empty?
+        n = Note.find_or_create_by(content: note)
+        self.notes << n
+      end
+    end
+  end
+
 end
